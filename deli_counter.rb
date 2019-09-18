d1152cd33e361cff3fcb5ef3c22b6baad137e526
Ashley Twomey <aprier091012@gmail.com>
2019-09-18 23:46:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,6 @@ def deli_line(a)
     puts "Th line is currently empty."
   else 
     a.each.with_index(1) do |name, index|
-      line_array.push("#{name}")
+      line_array.push("#{name}. #{index}")
+      
   
